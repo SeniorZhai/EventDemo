f87092dcb41df497deda813be679268a5d8f614f
仔仔 <370985116@qq.com>
2015-04-16 10:11:56 +0800

    first commit

---
+++ app/src/main/java/com/zoe/eventdemo/EventA.java
@@ -0,0 +1,11 @@
+package com.zoe.eventdemo;
+
+/**
+ * Created by zhaitao on 15/2/6.
+ */
+public class EventA {
+    String text = "asd";
+    public EventA(String text){
+        this.text = text;
+    }
+}
---
+++ app/src/main/java/com/zoe/eventdemo/MainActivity.java
@@ -0,0 +1,62 @@
+package com.zoe.eventdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import de.greenrobot.event.EventBus;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EventBus.getDefault().register(this);
+        new Thread(new Runnable() {
+            int i= 0;
+            @Override
+            public void run() {
+                while(true){
+                    try {
+                        Thread.sleep(2000);
+                        EventBus.getDefault().post(new EventA(i++*1111+""));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        EventBus.getDefault().unregister(this);
+        super.onDestroy();
+    }
+
+    public void onEventMainThread(EventA eventA){
+        TextView tv = (TextView) findViewById(R.id.tv);
+        tv.setText(eventA.text);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
